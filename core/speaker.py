c12523674ec25b213b3d033424ee5e442b27a311
jithender210 <jithenderrao5@gmail.com>
2026-08-18 23:32:14 +0530

    third

--- core/speaker.py
+++ core/speaker.py
@@ -1,6 +1,27 @@
-import pyttsx3
+import streamlit.components.v1 as components
+import json
+
 
 def speak(text):
-    engine=pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+    text = json.dumps(text)
+
+    components.html(
+        f"""
+        <script>
+            const text = {text};
+
+            if ("speechSynthesis" in window) {{
+                window.speechSynthesis.cancel();
+
+                const speech = new SpeechSynthesisUtterance(text);
+
+                speech.rate = 1.0;
+                speech.pitch = 1.0;
+                speech.volume = 1.0;
+
+                window.speechSynthesis.speak(speech);
+            }}
+        </script>
+        """,
+        height=0
+    )
